e7518791ad5ef0e4026be884715ab7e951042e2a
Julie Groth <groth.julie@gmail.com>
2018-09-20 18:27:24 -0700

    removed console log

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("materialize js connected");
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, 0);
   });
