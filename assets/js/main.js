94c64322b6a232055a654d071ae309aa4f7c4c8a
Julie Groth <groth.julie@gmail.com>
2018-10-03 10:51:46 -0700

    added sidenav

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, 0);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, 0);
+  });
