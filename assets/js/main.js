f9197647c50be145adbab49da5956cccba8bcce8
Julie Groth <groth.julie@gmail.com>
2018-10-03 11:55:47 -0700

    updated to jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, 0);
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, 0);
-  });
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+});
